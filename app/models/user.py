43ecd0a88efbe1d6a3e721fe7ac2c3afbc06afb3
Elly Wong <elly.yywong@gmail.com>
2022-01-27 13:32:51 -0800

    added endpoints of users

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,11 @@
+from enum import unique
 from sqlalchemy import VARCHAR
 from app import db
 from flask import current_app
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique= True, nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     secret = db.Column(db.String(100), nullable=False)
     class_name = db.Column(db.String(100))
@@ -12,7 +13,7 @@ class User(db.Model):
     about_me = db.Column(db.String(100))
     location = db.Column(db.String(100))
     
-    # constructor
+    # # constructor
     # def __init__(self, username, email, secret):
     #     self.username = username
     #     self.email = email
@@ -20,7 +21,7 @@ class User(db.Model):
         
     def make_user_json(self):
         return {
-                "user_id": self.user_id,
+                "user_id": self.id,
                 "username": self.username,
                 "email": self.email, 
                 "secret": self.secret
--- app/routes_user.py
+++ app/routes_user.py
@@ -25,3 +25,46 @@ def create_user():
             return {"details" : f"Request body must include email with string type."}, 400
         if "secret" in err.args:
             return {"details" : f"Request body must include secret with string type."}, 400
+
+
+@user_bp.route("", methods=["GET"])
+def get_all_users():
+    all_users = User.query.all()
+    all_users_response = [(user.make_user_json()) for user in all_users]
+    return jsonify(all_users_response), 200
+
+@user_bp.route("/<user_id>", methods=["GET"])
+def get_a_specific_users(user_id):
+    try: 
+        user = User.query.get(user_id)
+        return user.make_user_json(), 200;
+    except:
+        return  {"details": f"User {user_id} not found"}, 404
+
+@user_bp.route("/login/verify", methods=["GET"])
+def verify_a_specific_user():
+    request_body = request.get_json()[0]
+    # input_username = request_body['username']
+    input_email = request_body['email']
+    input_secret = request_body['secret']
+
+    if input_email:
+        user = User.query.filter_by(email = input_email).first()
+    # elif input_username:
+    #     user = User.query.filter(username = input_username).first()
+    if user:
+        if user.secret == input_secret:
+            return jsonify([{"message":"login successfully"}]), 200
+        else:
+            return jsonify([{"message":"invalid password"}]), 400
+    else:
+        return jsonify([{"message":"invalid username or email"}]), 400
+
+
+@user_bp.route("", methods=["DELETE"])
+def delete_all_user():
+    all_users = User.query.all()
+    for user in all_users:
+        db.session.delete(user)
+        db.session.commit()
+    return {"details": "all users were successfully deleted"}, 200
