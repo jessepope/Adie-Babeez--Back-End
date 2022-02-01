01521bec9caa569e3505ac60677f515473c0b888
Elly Wong <elly.yywong@gmail.com>
2022-01-31 17:20:46 -0800

    commented out CORS ;
    l
    ;

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
-from flask_cors import CORS
+# from flask_cors import CORS
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -37,5 +37,5 @@ def create_app(test_config=None):
     app.register_blueprint(post_bp)
 
 
-    CORS(app)
+    # CORS(app)
     return app
